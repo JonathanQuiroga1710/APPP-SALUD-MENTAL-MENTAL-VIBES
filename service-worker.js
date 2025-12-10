2c58c1bc70783ed13815fbb7025e7cf71a9f223d
JonathanQuiroga1710 <142407984+JonathanQuiroga1710@users.noreply.github.com>
2025-12-09 23:25:13 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
+// Service Worker sencillo para PWA de MentalVibes
+
+self.addEventListener("install", event => {
+  console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service Worker activado");
+});
+
+self.addEventListener("fetch", event => {
+  // Por ahora solo dejamos pasar todas las peticiones
+  event.respondWith(fetch(event.request));
+});
 
